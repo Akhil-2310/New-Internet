3c328fd50af87ce7a0320e5a772630e0ae3f2687
Akhil-2310 <akhilnanavati23@gmail.com>
2024-08-19 07:30:49 +0530

    frontend

---
+++ src/components/Home.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <a className="btn btn-ghost text-xl">DeComm</a>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <Link to="/marketplace">Marketplace</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+              className="max-w-sm rounded-lg shadow-2xl"
+            />
+            <div>
+              <h1 className="text-5xl font-bold">Box Office News!</h1>
+              <p className="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Marketplace.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react";
+import { Link } from "react-router-dom";
+
+// 1. Get projectId
+const projectId = "54c238d52f1218087ae00073282addb8";
+
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: "Ethereum",
+  currency: "ETH",
+  explorerUrl: "https://etherscan.io",
+  rpcUrl: "https://cloudflare-eth.com",
+};
+const sepolia = {
+  chainId: 11155111,
+  name: "Sepolia",
+  currency: "ETH",
+  explorerUrl: "https://sepolia.etherscan.io",
+  rpcUrl:
+    "https://eth-sepolia.g.alchemy.com/v2/_O9yEvZei4_BPgQbLawL754cAfubB8jr", // Replace with your Infura project ID
+};
+
+// 3. Create a metadata object
+const metadata = {
+  name: "My Website",
+  description: "My Website description",
+  url: "https://mywebsite.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.mywebsite.com/"],
+};
+
+// 4. Create Ethers config
+const ethersConfig = defaultConfig({
+  /*Required*/
+  metadata,
+
+  /*Optional*/
+  enableEIP6963: true, // true by default
+  enableInjected: true, // true by default
+  enableCoinbase: true, // true by default
+  rpcUrl: "...", // used for the Coinbase SDK
+  defaultChainId: 1, // used for the Coinbase SDK
+});
+
+// 5. Create a AppKit instance
+createWeb3Modal({
+  ethersConfig,
+  chains: [mainnet, sepolia],
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+});
+
+const Marketplace = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">DeComm</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link to="/list">List Products</Link>
+            </li>
+            <li>
+              <Link to="/my">My Products</Link>
+            </li>
+            <w3m-button />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Marketplace;
